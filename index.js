7aeb8984f20976c9b76b8f2a79e5fc2bfe906c4a
Ram-tyagi <ramtyagi04@gmail.com>
2023-12-05 22:04:50 +0530

    mockdata

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express=require('express')
+const users=require("./MOCK_DATA.json")
 
 const app=express()
 const PORT=8000
+
+app.get("/api/users",(req,res)=>{   
+    return res.json(users)
+})
 
 app.listen(PORT,() => {
     console.log(`server is running on port ${PORT}`)
